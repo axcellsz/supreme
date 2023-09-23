d24f3011047520eb7542e3ae4287fb5f91fcde49
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-09-24 01:35:55 +0700

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -1,83 +1,193 @@
-
 #!/bin/bash
-
-while getopts u:e:s: flag
-do
-    case "${flag}" in
-        u) user=${OPTARG};;
-        e) masaaktif=${OPTARG};;
-        s) sni=${OPTARG};;
-    esac
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+domain=$(cat /etc/xray/domain)
+clear
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+    clear
+    echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\e[42m         Buat akun vmess          \E[0m"
+    echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    
+    read -rp "User: " -e user
+    CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+    
+    if [[ ${CLIENT_EXISTS} == '1' ]]; then
+        clear
+        echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\e[42m         Buat akun vmess          \E[0m"
+        echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo ""
+        echo "Nama tersebut sudah digunakan, masukan nama lain!"
+        echo ""
+        echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        read -n 1 -s -r -p "Press any key to back on menu"
+        menu
+    fi
 done
 
-source /var/lib/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/xray/domain)
-else
-domain=$IP
-fi
-
-CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json)
-
-if [[ -n  "$CLIENT_EXISTS"  ]]; then echo "err"; exit 0; fi
-
-
 uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmess$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
-asu=`cat<<EOF
-     {
-      "add": "${domain}", 
-      "host": "${sni}", 
-      "aid": 0, 
-      "path": "/vmess", 
-      "net": "ws", 
-      "ps": "${user}", 
-      "tls": "tls", 
-      "type": "none", 
-      "port": "443", 
-      "v": "2", 
-      "id": "${uuid}"
-}
-EOF`
-ask=`cat<<EOF
-     {
-     "tls": "none",
-     "v": "2", 
-     "host": "${domain}",
-     "type": "", 
-     "ps": "${user}", 
-     "port": 80, 
-     "net": "ws", 
-     "id": "${uuid}", 
-     "path": "/vmess", 
-     "add": "${sni}", 
-     "aid": 0
-}
-EOF`
-
-vmesslink1="vmess://$(echo $asu | base64 -w 0)"
-vmesslink2="vmess://$(echo $ask | base64 -w 0)"
-
-systemctl restart xray > /dev/null 2>&1
-service cron restart > /dev/null 2>&1
-result=`cat<<EOF
+VMESS_WS=`cat<<EOF
       {
-      "Host":"${domain}",
-      "User":"${user}",
-      "UUID":"${uuid}",
-      "PortTLS":"443",
-      "NoneTLS":"80",
-      "Vmess":"WS",
-      "Path":"/vmess",
-      "LinkTLS":"${vmesslink1}",
-      "LinknoneTLS":"${vmesslink2}",
-      "ExpiredOn":"$exp"
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
 }
 EOF`
+VMESS_NON_TLS=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "80",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF`
+VMESS_GRPC=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "vmess-grpc",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+cat > /var/www/html/vmess-$user.txt <<-END
+_______________________________________________________
+              Format Vmess WS (CDN)
+_______________________________________________________
 
-echo $result | tee -a /etc/log-create-user.log;
+- name: Vmess-$user-WS (CDN)
+  type: vmess
+  server: ${domain}
+  port: 443
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  udp: true
+  tls: true
+  skip-cert-verify: true
+  servername: ${domain}
+  network: ws
+  ws-opts:
+    path: /vmess
+    headers:
+      Host: ${domain}
+_______________________________________________________
+              Format Vmess WS (CDN) Non TLS
+_______________________________________________________
+
+- name: Vmess-$user-WS (CDN) Non TLS
+  type: vmess
+  server: ${domain}
+  port: 80
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  udp: true
+  tls: false
+  skip-cert-verify: false
+  servername: ${domain}
+  network: ws
+  ws-opts:
+    path: /vmess
+    headers:
+      Host: ${domain}
+_______________________________________________________
+              Format Vmess gRPC (SNI)
+_______________________________________________________
+
+- name: Vmess-$user-gRPC (SNI)
+  server: ${domain}
+  port: 443
+  type: vmess
+  uuid: ${uuid}
+  alterId: 0
+  cipher: auto
+  network: grpc
+  tls: true
+  servername: ${domain}
+  skip-cert-verify: true
+  grpc-opts:
+    grpc-service-name: vmess-grpc
+
+_______________________________________________________
+              Link Vmess Account
+_______________________________________________________
+Link TLS : vmess://$(echo $VMESS_WS | base64 -w 0)
+_______________________________________________________
+Link none TLS : vmess://$(echo $VMESS_NON_TLS | base64 -w 0)
+_______________________________________________________
+Link GRPC : vmess://$(echo $VMESS_GRPC | base64 -w 0)
+_______________________________________________________
+
+
+
+END
+
+vmesslink1="vmess://$(echo $VMESS_WS | base64 -w 0)"
+vmesslink2="vmess://$(echo $VMESS_NON_TLS | base64 -w 0)"
+vmesslink3="vmess://$(echo $VMESS_GRPC | base64 -w 0)"
+systemctl restart xray
+systemctl restart nginx
+service cron restart
+clear
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[42mThank You For Using Our Services\E[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[42mXray/Vmess Account              \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Domain : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port TLS : 443" | tee -a /etc/log-create-user.log
+echo -e "Port none TLS : 80" | tee -a /etc/log-create-user.log
+echo -e "Port  GRPC : 443" | tee -a /etc/log-create-user.log
+echo -e "id : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "alterId : 0" | tee -a /etc/log-create-user.log
+echo -e "Security : auto" | tee -a /etc/log-create-user.log
+echo -e "Network : WS or gRPC" | tee -a /etc/log-create-user.log
+echo -e "Path TLS : can use anything example :(/free) " | tee -a /etc/log-create-user.log
+echo -e "Path Non TLS : can use anything example :(/free) " | tee -a /etc/log-create-user.log
+echo -e "ServiceName : vmess-grpc" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link TLS :
+${vmesslink1}" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link none TLS :
+${vmesslink2}" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC :
+${vmesslink3}" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Format OpenClash : http://${domain}:81/vmess-$user.txt" | tee -a /etc/log-create-user.log
+echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
